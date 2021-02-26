7e4e39f4e3670b40e205664c9757a0d2d03f682c
nicolas <a2018115260@estudiantes.upsa.edu.bo>
2021-02-25 20:04:04 -0400

    se creo el controlador calculator y se crearon Add, Multiply, Divide, Subtract, en post

---
+++ SolCalculadora/ApiCalculadora/Controllers/CalculatorController.cs
@@ -0,0 +1,34 @@
+﻿namespace ApiCalculadora.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Add")]
+        public int Add([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa + numb;
+        }
+        [HttpPost]
+        [Route("Multiply")]
+        public int Multiply([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa * numb;
+        }
+        [HttpPost]
+        [Route("Subtract")]
+        public int Subtract([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa - numb;
+        }
+        [HttpPost]
+        [Route("Division")]
+        public int Division([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa / numb;
+        }
+    }
+}
