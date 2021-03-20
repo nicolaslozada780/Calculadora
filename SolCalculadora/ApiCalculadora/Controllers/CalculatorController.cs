7bf96a56a05e84fbad86479fe1aa83f2b6a1ab74
nicolas <a2018115260@estudiantes.upsa.edu.bo>
2021-03-20 11:49:24 -0400

    Se logro suma

--- SolCalculadora/ApiCalculadora/Controllers/CalculatorController.cs
+++ SolCalculadora/ApiCalculadora/Controllers/CalculatorController.cs
@@ -20,52 +20,6 @@
             return numa + numb;
         }
 
-        [HttpPost]
-        [Route("Multiply")]
-        public int Multiply([FromHeader] int numa, [FromHeader] int numb)
-        {
-            return numa * numb;
-        }
-
-        [HttpGet]
-        [Route("Multiply")]
-        public int Multiply2(int numa,int numb)
-        {
-            return numa * numb;
-        }
-
-
-        [HttpPost]
-        [Route("Subtract")]
-        public int Subtract([FromHeader] int numa, [FromHeader] int numb)
-        {
-            return numa - numb;
-        }
-
-        [HttpGet]
-        [Route("Subtract")]
-        public int Subtract2(int numa,int numb)
-        {
-            return numa - numb;
-        }
-
-        [HttpPost]
-        [Route("Division")]
-        public int Division([FromHeader] int numa, [FromHeader] int numb)
-        {
-            if (numb == 0)
-                return 0;
-            return numa / numb;
-        }
-
-        [HttpGet]
-        [Route("Division")]
-        public int Division2(int numa,int numb)
-        {
-            if (numb == 0)
-                return 0;
-            return numa / numb;
-        }
-
+       
     }
 }
